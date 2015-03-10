8df5c2faa960af446807b707e166f00ce5b413c3
afgiel <agiel@stanford.edu>
2015-03-10 01:43:00 -0700

    cleaned up

--- graph_segment.py
+++ graph_segment.py
@@ -9,29 +9,24 @@ from skimage.filters import gaussian_filter
 from skimage import data
 from skimage import io
 
-im = rescale(io.imread('data/blackhole.jpg',as_grey=True), .2)
-im_noblur = rescale(io.imread('data/blackhole.jpg',as_grey=True), .2)
-im = gaussian_filter(im, 4)
-#im = rgb2gray(im)
-print im.shape
+IMAGE_PATH = 'data/puppy1.jpeg'
+k = 70.0 # Constant for the tau function
+RESCALE_FACTOR = 0.1
+GAUSSIAN_BLUR = 4
 
-k = 80.0 # Constant for the tau function
 
-G = nx.grid_graph(dim=list(im.shape[:2]))
+def get_graph_from_image(image): 
+  grid = nx.grid_graph(dim=list(image.shape[:2])) 
 
-segment_graph = nx.Graph()
-segment_graph.add_nodes_from(G.nodes())
-
-for u,v,d in G.edges(data=True):
-    d['weight'] = np.abs(im[u] - im[v])*255
-
-sorted_edges = sorted(G.edges(data=True), key=lambda (u,v,d): d['weight'])
+  for u,v,d in grid.edges(data=True):
+    d['weight'] = np.abs(image[u] - image[v])*255
+ 
+  return grid 
 
 '''
 Must pass in the connected nodes of the vertex
 '''
-def max_diff(nodes):
-    global segment_graph
+def max_diff(nodes, segment_graph):
     # Degenerate case
     if len(nodes) == 1:
         return 0.0
@@ -39,29 +34,65 @@ def max_diff(nodes):
     edge = max(segment_graph.edges(nodes,data=True), key=lambda (u,v,d): d['weight'])
     return edge[2]['weight']
 
-def min_internal_diff(nodes1, nodes2):
+
+def min_internal_diff(nodes1, nodes2, graph):
     global k
     tauC1 = k/len(nodes1)
     tauC2 = k/len(nodes2)
 
-    return min(max_diff(nodes1) + tauC1, max_diff(nodes2) + tauC2)
+    return min(max_diff(nodes1, graph) + tauC1, max_diff(nodes2, graph) + tauC2)
 
-print "starting algo"
-for u,v,d in sorted_edges:
+
+def segment(segment_graph, sorted_edges):
+  for u,v,d in sorted_edges:
     connected1 = nx.node_connected_component(segment_graph, u)
     connected2 = nx.node_connected_component(segment_graph, v)
-    if d['weight'] <= min_internal_diff(connected1, connected2):
-        segment_graph.add_edge(u,v,weight=d['weight'])
+    if d['weight'] <= min_internal_diff(connected1, connected2, segment_graph):
+      segment_graph.add_edge(u,v,weight=d['weight'])
+  return segment_graph
 
-group = 1
-group_img = np.zeros(im.shape)
-print "Drawing image"
-for subgraph in nx.connected_component_subgraphs(segment_graph):
+
+def visualize(image_no_blur, segment_graph):
+  group = 1
+  group_img = np.zeros(image_no_blur.shape)
+
+  for subgraph in nx.connected_component_subgraphs(segment_graph):
     for node in subgraph.nodes():
-        group_img[node] = group
+      group_img[node] = group
     group += 1
-print "matplotlibbin"
 
-plt.imshow(im_noblur, cmap=plt.cm.gray)
-plt.matshow(group_img)
-plt.show()
+  plt.imshow(image_no_blur, cmap=plt.cm.gray)
+  plt.matshow(group_img)
+  plt.show()
+
+
+def main():
+
+  print 'loading image and preprocessing'
+  # image preprocessing 
+  image = rescale(io.imread(IMAGE_PATH, as_grey=True), RESCALE_FACTOR)
+  image_no_blur = image
+  image = gaussian_filter(image, GAUSSIAN_BLUR)
+  print 'image of size ' + str(image.shape) + ' after resizing'
+
+  # k sanity check 
+  print 'k = %.2f - should be approx %.2f' % (k, np.mean(image.shape))
+
+  # make graphs from image
+  print 'constructing graph'
+  grid_graph = get_graph_from_image(image)
+  segment_graph = nx.Graph()
+  segment_graph.add_nodes_from(grid_graph.nodes())
+  sorted_edges = sorted(grid_graph.edges(data=True), key=lambda (u,v,d): d['weight'])
+
+  # run algo 
+  print 'segmenting image graph'
+  segment_graph = segment(segment_graph, sorted_edges)
+  
+  # visualize
+  print 'visualizing'
+  visualize(image_no_blur, segment_graph)
+
+
+if __name__ == '__main__':
+  main()
